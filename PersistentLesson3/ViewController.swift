a6ab2a5ce1fb5c917aae316f1745eca11c6fe0d7
Yan-ta <yatashe@yandex.ru>
2022-06-01 20:31:48 +0300

    Initial Commit

---
+++ PersistentLesson3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PersistentLesson3
+//
+//  Created by Ян Таше on 01.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
